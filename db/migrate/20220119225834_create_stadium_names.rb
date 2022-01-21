2e68d9efe62f60bea6b968404214652aafabea53
Colten Appleby <colten.appleby@gmail.com>
2022-01-21 16:20:20 -0500

    added stadium name to migration

--- db/migrate/20220119225834_create_stadium_names.rb
+++ db/migrate/20220119225834_create_stadium_names.rb
@@ -1,6 +1,7 @@
 class CreateStadiumNames < ActiveRecord::Migration[6.1]
   def change
     create_table :stadium_names do |t|
+      t.string :name
       t.references :stadium, null: false, foreign_key: true
 
       t.timestamps
