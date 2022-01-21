dc007d8d3a1f4053ba715ee9481607811735bb53
Colten Appleby <colten.appleby@gmail.com>
2022-01-21 16:27:36 -0500

    created seed for Fenway Park

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+City.create(name: 'Boston', state: 'MA', country: 'US')
+Stadium.create(address: '34 Big Papi Way', 
+    latitude: '42°20′46.5″N', 
+    longitude: ' 71°5′51.9″W', 
+    city: City.first, 
+    year_built: 1912, 
+    wikipedia: 'https://en.wikipedia.org/wiki/Fenway_Park')
+StadiumName.create(stadium: Stadium.first, name: 'Fenway Park')
+Team.create(name: "Boston Red Sox")
+TeamCity.create(city: City.first, team: Team.first)
+StadiumTeam.create(stadium: Stadium.first, team: Team.first)
